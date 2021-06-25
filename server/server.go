8bfb3f4154f2e4bc4eab30ae230e443458e45fb5
jason <uniqueacheng@gmail.com>
2021-06-25 18:55:33 +0800

    参数结构体、返回值结构体指针、json序列化

--- server/server.go
+++ server/server.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+type Input struct {
+	Name string
+}
+
+type Output struct {
+	Msg string
+}
+
 func myHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello "+ r.URL.Path[1:] + "!\n")
+	data,_ := ioutil.ReadAll(r.Body)
+	input := &Input{}
+	_ = json.Unmarshal(data,input)
+	output, _ := json.Marshal(&Output{
+		Msg: "hello, " + input.Name,
+	})
+
+	fmt.Fprintf(w,"%s",string(output))
 }
 
 func main() {
