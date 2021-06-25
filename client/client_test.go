929fe2ddab49977cf84321206f0018a88de28880
jason <uniqueacheng@gmail.com>
2021-06-25 20:48:05 +0800

    利用反射给结构体赋值

--- client/client_test.go
+++ client/client_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -15,8 +16,19 @@ func TestSetFuncField(t *testing.T) {
 	fmt.Println(newAdd)
 	newDelete, _ := Delete(s, 5)
 	fmt.Println(newDelete)
-}
 
+	u := &User{}
+	v := reflect.ValueOf(u).Elem()
+	v.FieldByName("Name").SetString("tome")
+	v.FieldByName("Age").SetInt(18)
+	v.FieldByName("Say").Set(reflect.MakeFunc(v.FieldByName("Say").Type(), func(args []reflect.Value) (results []reflect.Value) {
+		fmt.Println("hello world")
+		return []reflect.Value{reflect.ValueOf([]interface{}{})}
+	}))
+	res := u.Say()
+	fmt.Println(res)
+	fmt.Println(u)
+}
 
 // 指定索引添加元素
 func Add(values []interface{}, value interface{}, index int) ([]interface{}, error) {
@@ -36,9 +48,15 @@ func Add(values []interface{}, value interface{}, index int) ([]interface{}, err
 // 指定索引删除元素
 func Delete(values []interface{}, index int) ([]interface{}, error) {
 	if index < 0 || index >= len(values) {
-		return nil,errors.New("index 非法")
+		return nil, errors.New("index 非法")
 	}
 	var res []interface{}
 	res = append(values[:index], values[index+1:]...)
 	return res, nil
+}
+
+type User struct {
+	Name string
+	Age  int
+	Say  func() []interface{}
 }
